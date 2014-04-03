9c2f2ff90a6f16ab36116fcd0c2fa933d2aa734b
Guy Muff <Guy.Muff@bazaarvoice.com>
2014-04-03 16:48:23 -0700

    Latest "learning" data

--- redeye/ConvertToCSV.groovy
+++ redeye/ConvertToCSV.groovy
@@ -1,6 +1,7 @@
 import groovy.json.JsonSlurper
 import org.codehaus.groovy.grails.web.json.JSONObject
 import redeye.Author
+import redeye.Product
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -28,12 +29,25 @@ while (readline) {
     JSONObject root = slurper.parseText(readline)
 
     Author author = Author.findByIdString(root['AuthorId'])
+    Product product = Product.findByProduct_id(root['ProductId'])
 
-    if(author) {
-        Date lastModified = format.parse(root.LastModificationTime)
-        String line = "${author.id},${root['ProductId']},${root['Rating']},${lastModified.time}\n"
-        w.write(line)
+    if (!author) {
+
+        Date submissionTime = format.parse(root['SubmissionTime'])   // TODO this not 100% accurate in some cases
+        Date moderatedTime = format.parse(root['LastModeratedTime'])
+        author = new Author(idString: root['AuthorId'], nickName: root['AuthorId'],
+                lastModeratedTime: moderatedTime, submissionTime: submissionTime)
+
+        author.save()
     }
+    if (product) {
+        Date lastModified = format.parse(root.LastModificationTime)
+        String line = "${author.id},${product.id},${root['Rating']},${lastModified.time}\n"
+        w.write(line)
+    } else {
+        System.err.println("No product ${root['ProductId']}")
+    }
+
 
 
     readline = r.readLine()
